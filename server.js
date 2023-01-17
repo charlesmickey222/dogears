e77a330fddb465840f5d2de77f3ddb16cdfac8f2
CharlieMickey <charles.f.mickey@gmail.com>
2023-01-17 10:55:25 -0600

    add author view files

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ import'./config/passport.js'
 import { router as indexRouter } from './routes/index.js'
 import { router as authRouter } from './routes/auth.js'
 import { router as booksRouter } from './routes/books.js'
+import { router as authorsRouter } from './routes/authors.js'
 
 // create the express app
 const app = express()
@@ -67,6 +68,7 @@ app.use(passDataToView)
 app.use('/', indexRouter)
 app.use('/auth', authRouter)
 app.use('/books', booksRouter)
+app.use('/authors', authorsRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
