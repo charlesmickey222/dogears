e5f4453161f8ab70018fdb7a40236d20c8737d3b
CharlieMickey <charles.f.mickey@gmail.com>
2023-01-17 11:44:00 -0600

    add index and create for authors

---
+++ controllers/authors.js
@@ -0,0 +1,26 @@
+import { Author } from "../models/author.js";
+
+function index(req,res){
+Author.find({})
+.then(authors=>{
+  res.render('authors/index',{
+    title:'Authors',
+    authors:authors,
+  })
+})
+}
+
+function create(req,res){
+  req.body.poster = req.user.profile._id
+  req.body.living = Boolean(req.body.living)
+  Author.create(req.body)
+  .then(author=>{
+    res.redirect('/authors')
+  })
+}
+
+
+export{
+index,
+create,
+}
--- routes/authors.js
+++ routes/authors.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import { isLoggedIn } from "../middleware/middleware.js";
+import * as authorsCtrl from '../controllers/authors.js'
 
 const router = Router();
 
+router.get('/', authorsCtrl.index)
+
+router.post('/', isLoggedIn, authorsCtrl.create)
 export{
   router
 }
