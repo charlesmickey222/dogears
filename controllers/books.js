b6d8bad0a4c0a71479d31d5a757201c9b9b288f6
CharlieMickey <charles.f.mickey@gmail.com>
2023-01-17 14:06:59 -0600

    add error handling to books ctrllr

--- controllers/books.js
+++ controllers/books.js
@@ -8,12 +8,20 @@ function index(req,res){
       books,
     })
   })
+  .catch(err=>{
+    console.log(err)
+    res.redirect('/books')
+  })
 }
 
 function create(req,res){
   req.body.poster = req.user.profile._id
   Book.create(req.body)
   .then(book=>{
+    res.redirect('/books')
+  })
+  .catch(err=>{
+    console.log(err)
     res.redirect('/books')
   })
 }
@@ -26,12 +34,30 @@ function show(req,res){
       book:book,
     })
   })
+  .catch(err=>{
+    console.log(err)
+    res.redirect('/books')
+  })
 }
 
 function deleteBook(req,res){
   Book.findByIdAndDelete(req.params.id)
   .then(book=>{
     res.redirect('/books')
+  })
+  .catch(err=>{
+    console.log(err)
+    res.redirect('/books')
+  })
+}
+
+function edit(req,res){
+  Book.findById(req.params.id)
+  .then(book=>{
+    res.render('books/edit',{
+      title:'update book information',
+      book:book,
+    })
   })
   .catch(err=>{
     console.log(err)
